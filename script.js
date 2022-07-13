390af1050040d0846c8047433ae3d56bd8c49020
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-07-13 17:02:19 +0530

    feature: add logic to replace words

--- script.js
+++ script.js
@@ -24,10 +24,72 @@ const readCSVToObj = async (filePath) => {
     return hashMap
 }
 
+//convert file->array
+const readFileToArray = (filePath) => {
+    const fileContents = fs.readFileSync(filePath, 'utf-8')
+    // console.log(typeof fileContents)
+    const contentArray = fileContents.split(/\r?\n/)
+    return contentArray
+    // return fileContents
+}
+
+const replaceWords = (FileLineArray, EnglishToFrench, EnglishToCount) => {
+    let finalFileLines = []
+    let allWords = Object.keys(EnglishToFrench)
+    for (line of FileLineArray) {
+        let FileWords = line.split(" ")
+        for (word of FileWords) {
+            let currentWord = word
+            let testWord = word.replace(/[`~!@#$%^&*()_|+\-=?;:",.<>\{\}\[\]\\\/]/gi, '')
+            if (allWords.includes(testWord.toLowerCase())) {
+                console.log(checkWordFormat(testWord))
+                let wordFormat = checkWordFormat(testWord)
+                let equivalentFrenchWord = EnglishToFrench[testWord.toLowerCase()]
+                switch (wordFormat) {
+                    case 0:
+                        currentWord = currentWord = currentWord.replace(testWord, equivalentFrenchWord.toLowerCase())
+                        break
+                    case 1:
+                        currentWord = currentWord = currentWord.replace(testWord, capitalizeWord(equivalentFrenchWord))
+                        break
+                    case 2:
+                        currentWord = currentWord = currentWord.replace(testWord, equivalentFrenchWord.toUpperCase())
+                        break
+                    default:
+                        break
+                }
+                console.log(word, currentWord)
+            }
+        }
+    }
+
+}
+
+const checkWordFormat = (word) => {
+    // 0 -> lowercase
+    // 1 -> capitalized
+    // 2 -> uppercase
+    if (word === word.toLowerCase()) {
+        return 0
+    } else if (word === word.toUpperCase()) {
+        return 2
+    } else {
+        return 1
+    }
+}
+
+const capitalizeWord = (word) => {
+    return word[0].toUpperCase() + word.substring(1)
+}
+
+
 //contains main flow
 const main = async () => {
     const EnglishToFrench = await readCSVToObj('./input_files/french_dictionary.csv')
     const EnglishToCount = readTextFileToObj('./input_files/find_words.txt')
+    const InputFileArray = readFileToArray('./input_files/t8.shakespeare.txt')
+    replaceWords(InputFileArray, EnglishToFrench, EnglishToCount)
+    // console.log(InputFileArray)
 
 }
 
